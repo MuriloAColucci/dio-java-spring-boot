e1ba7d01298521ce854cf70c505b2214fda064fe
murilo <murilocolucci@gmail.com>
2025-05-12 10:30:41 -0300

    initial commit

---
+++ spring-ioc-di-beans-autowired/src/main/java/dio/springboot/SpringIocDiBeansAutowiredApplication.java
@@ -0,0 +1,13 @@
+package dio.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIocDiBeansAutowiredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIocDiBeansAutowiredApplication.class, args);
+	}
+
+}
