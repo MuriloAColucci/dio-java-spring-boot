493138212551641c5ef7a7391a2385e9342e5aec
murilo <murilocolucci@gmail.com>
2025-05-14 14:17:48 -0300

    Configurações para o Railwail

--- Projetos/santander-dev-week/src/main/java/me/dio/controller/exception/GlobalExceptionHandler.java
+++ Projetos/santander-dev-week/src/main/java/me/dio/controller/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package me.dio.controller.exception;
 
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
 
+@Hidden
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
